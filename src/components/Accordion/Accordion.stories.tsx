fe1e3b9b937eca103e0a19856fc24c13d02b7fc9
Maksim Filanovich <maxim_filanovich@mail.ru>
2023-01-21 17:33:51 +0300

    init: init storybook

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import type { Meta, Story } from '@storybook/react';
+import {Accordion, AccordionProps} from "./Accordion";
+
+export default {
+  title: 'components/Accordion',
+  component: Accordion,
+  args: {
+    isOpen: false,
+    title: 'My title',
+    children: 'Content'
+  },
+  parameters: {
+    controls: {
+      exclude: ['innerControl', 'onToggle', 'isOpen']
+    }
+  }
+} as Meta<AccordionStoryProps>
+
+type AccordionStoryProps = Pick<AccordionProps, 'title' | 'children'>
+
+export const AccordionStoryTemplate: Story<AccordionStoryProps> = ({ ...args }) => (
+  <Accordion {...args} innerControl />
+)
+
+AccordionStoryTemplate.storyName = 'Accordion'
